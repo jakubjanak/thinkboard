cd3fadcb126c5a1a4141a3da2b0a4c68459e0e65
Jakub Janák <jakub.janak@icloud.com>
2026-05-23 22:57:18 +0200

    RateLimitedUI component ready. Now I have to resolve CORS issue because of trying to fetch with axios notes from database.

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,33 @@
 import Navbar from "../components/Navbar";
+import { useState, useEffect } from "react";
+import RateLimitedUI from "../components/RateLimitedUI";
+import axios from "axios";
 
 const Homepage = () => {
-  return <Navbar />;
+  const [rateLimited, setRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        console.log(res.data);
+      } catch (error) {
+        console.error("Error fetching the notes.", error);
+      }
+    };
+
+    fetchNotes();
+  }, []);
+
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+
+      {rateLimited && <RateLimitedUI />}
+    </div>
+  );
 };
 
 export default Homepage;
