908a9f527332ec28844ab998570379d43f62e90e
Jakub Janák <jakub.janak@icloud.com>
2026-05-24 00:48:00 +0200

    CreatePage component done. Also I have created axios.create in axios.js file.

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import { useState, useEffect } from "react";
 import RateLimitedUI from "../components/RateLimitedUI";
-import axios from "axios";
+import api from "../lib/axios";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
 
@@ -13,7 +13,7 @@ const Homepage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes");
+        const res = await api.get("/notes");
         console.log(res.data);
         setNotes(res.data);
       } catch (error) {
