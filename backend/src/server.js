6bec44d5e945d91a96c7b0766c3e1e98a4f8a4f0
Jakub Janák <jakub.janak@icloud.com>
2026-05-23 17:27:12 +0200

    ratelimiter implemented, backend done

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,16 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/ratelimiter.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 // middleware
 app.use(express.json()); // getting us access to req.body
+// ratelimiter for users
+app.use(rateLimiter);
 // simple custom middleware
 app.use((req, res, next) => {
   console.log(`Request method is ${req.method} & request URL is ${req.url}`);
@@ -19,6 +20,8 @@ app.use((req, res, next) => {
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log("Server started on port 5001");
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on port 5001");
+  });
 });
